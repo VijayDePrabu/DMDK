e87c61c53b26bbc2b3880a6e7c9f26a53c8b59fb
Vijay De <vijayde3@gmail.com>
2016-10-14 22:16:42 +0530

    Initial commit

---
+++ app/src/main/java/com/de/dmdk/MemberShipFormActivity.java
@@ -0,0 +1,13 @@
+package com.de.dmdk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MemberShipFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member_ship_form);
+    }
+}
